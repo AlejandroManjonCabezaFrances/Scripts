0349ec5ca480ec44d2b295be57fdd3cdd84277fd
AlejandroManjonCabezaFrances <manjon.alejandro@gmail.com>
2024-12-26 20:14:45 +0100

    script update (Makefile and Colors.hpp ok)

--- scriptClassC++/scriptClass.cpp
+++ scriptClassC++/scriptClass.cpp
@@ -4,36 +4,19 @@
 #include <algorithm> // Para std::transform
 #include <filesystem> // Para std::filesystem::create_directories c++17
 
-/**
- * Checkea si existe directorio "srcs" y lo crea a su altura jerárquica
-*/
-void    createFileCpp()
+std::string howManyClasses()
 {
-	std::filesystem::path srcsDir = std::filesystem::current_path() /".."/".."/"srcs";
-	if (!std::filesystem::exists(srcsDir))
-		std::filesystem::create_directories(srcsDir);
-}
+	std::string classesNumber;
 
-void   createCpp(std::string className)
-{
-
-	createFileCpp();
-
-	std::ofstream file("../../srcs/" + className + ".cpp");
-	if (!file)
+	while (1)
 	{
-		std::cerr << "Error creating .cpp!" << std::endl;
-		return;
+		std::cout << "Introduce el número de clases que quieres crear: " << std::endl;
+		std::cin >> classesNumber;
+		if (classesNumber.size() == 1 && classesNumber[0] >= '1' && classesNumber[0] <= '9')
+			break;
 	}
-
-	file << "#include \"../include/" << className << ".hpp\"\n\n";
-	file << className << "::" << className << "()\n" << "{\n" << "}\n\n";	//Constructor
-	file << className << "::" << className << "()\n" << "{\n" << "}\n\n";	//Constructor parameter
-	file << className << "::" << className << "(const " << className << "& constrCopy)\n{\n\n}\n\n";	//Copy constructor
-	file << className << "& " << className << "::operator=(const " << className << "& constrCopy)\n";	//Assigned operator
-	file << "{\n    if (this != &constrCopy)\n    {\n\n    }\n\n    return (*this);\n}\n\n";
-	file << className << "::" << "~" << className << "()\n" << "{\n" << "}\n\n";	//Destructor
-} 
+	return (classesNumber);
+}
 
 /**
  * Checkea si existe directorio "include" y lo crea a su altura jerárquica
@@ -80,18 +63,142 @@ std::string	createClassHpp(std::string className)
 	return (className);
 }
 
-std::string howManyClasses()
+/**
+ * Checkea si existe directorio "srcs" y lo crea a su altura jerárquica
+*/
+void    createFileCpp()
 {
-	std::string classesNumber;
+	std::filesystem::path srcsDir = std::filesystem::current_path() /".."/".."/"srcs";
+	if (!std::filesystem::exists(srcsDir))
+		std::filesystem::create_directories(srcsDir);
+}
 
-	while (1)
+void   createClassCpp(std::string className)
+{
+
+	createFileCpp();
+
+	std::ofstream file("../../srcs/" + className + ".cpp");
+	if (!file)
 	{
-		std::cout << "Introduce el número de clases que quieres crear: " << std::endl;
-		std::cin >> classesNumber;
-		if (classesNumber.size() == 1 && classesNumber[0] >= '1' && classesNumber[0] <= '9')
-			break;
+		std::cerr << "Error creating .cpp!" << std::endl;
+		return;
 	}
-	return (classesNumber);
+
+	file << "#include \"../include/" << className << ".hpp\"\n\n";
+	file << className << "::" << className << "()\n" << "{\n" << "}\n\n";	//Constructor
+	file << className << "::" << className << "()\n" << "{\n" << "}\n\n";	//Constructor parameter
+	file << className << "::" << className << "(const " << className << "& constrCopy)\n{	\n}\n\n";	//Copy constructor
+	file << className << "& " << className << "::operator=(const " << className << "& constrCopy)\n";	//Assigned operator
+	file << "{\n    if (this != &constrCopy)\n    {\n\n    }\n\n    return (*this);\n}\n\n";
+	file << className << "::" << "~" << className << "()\n" << "{\n" << "}\n";	//Destructor
+} 
+
+void	createMakefile(std::string className)
+{
+    std::ofstream file("../../Makefile");
+
+    if (!file)
+    {
+        std::cerr << "Error creating Makefile!" << std::endl;
+        return;
+    }
+
+    file << "NAME = a.out\n";
+    file << "CC = c++\n";
+    file << "CFLAGS = -Wall -Wextra -Werror -std=c++98\n";
+    file << "SRCS = srcs/main.cpp srcs/" << className << ".cpp\n";  // Usar el nombre de la clase
+    file << "DEBUG = -g3 -fsanitize=address\n";
+    file << "OBJS = $(SRCS:srcs/%.cpp=objs/%.o)\n\n";
+    file << "# Construction of the executable and update obj if necessary\n";
+    file << "$(NAME): objs $(OBJS)\n";
+    file << "\t$(CC) $(CFLAGS) $(DEBUG) $(OBJS) -o $(NAME)\n\n";
+    file << "all: $(NAME)\n\n";
+    file << "# Construction of object files (.o) from source files (.cpp)\n";
+    file << "objs/%.o: srcs/%.cpp\n";
+    file << "\t@$(CC) $(CFLAGS) -c $< -o $@\n\n";
+    file << "objs:\n";
+    file << "\t@mkdir -p objs/\n\n";
+    file << "clean:\n";
+    file << "\t@rm -rf objs\n\n";
+    file << "fclean: clean\n";
+    file << "\t@rm -f $(NAME)\n\n";
+    file << "re: fclean all\n\n";
+    file << ".PHONY: all clean fclean re\n";
+
+    file.close();
+}
+
+void createColorsHpp(std::string className)
+{
+    std::ofstream file("../../include/Colors.hpp");
+
+    if (!file)
+    {
+        std::cerr << "Error creating Colors.hpp!" << std::endl;
+        return;
+    }
+
+    file << "#ifndef COLORS_HPP\n";
+    file << "#define COLORS_HPP\n\n";
+
+    file << "/* ------- COLORS ------- */\n";
+    file << "#define BLACK   \"\\x1B[30m\"\n";
+    file << "#define RED     \"\\x1b[31m\"\n";
+    file << "#define GREEN   \"\\x1b[32m\"\n";
+    file << "#define YELLOW  \"\\x1b[33m\"\n";
+    file << "#define BLUE    \"\\x1b[34m\"\n";
+    file << "#define MAGENTA \"\\x1b[35m\"\n";
+    file << "#define CYAN    \"\\x1b[36m\"\n";
+    file << "#define WHITE   \"\\x1B[37m\"\n";
+    file << "#define ORANGE  \"\\x1B[38;2;255;128;0m\"\n";
+    file << "#define ROSE    \"\\x1B[38;2;255;151;203m\"\n";
+    file << "#define LBLUE   \"\\x1B[38;2;53;149;240m\"\n";
+    file << "#define LGREEN  \"\\x1B[38;2;17;245;120m\"\n";
+    file << "#define GRAY    \"\\x1B[38;2;176;174;174m\"\n";
+    file << "#define PURPLE  \"\\x1B[38;2;128;0;128m\"\n";
+    file << "#define BROWN   \"\\x1B[38;2;165;42;42m\"\n";
+    file << "#define PINK    \"\\x1B[38;2;255;192;203m\"\n";
+    file << "#define LIME    \"\\x1B[38;2;0;255;0m\"\n";
+    file << "#define INDIGO  \"\\x1B[38;2;75;0;130m\"\n";
+    file << "#define TEAL    \"\\x1B[38;2;0;128;128m\"\n";
+    file << "#define OLIVE   \"\\x1B[38;2;128;128;0m\"\n";
+    file << "#define VIOLET  \"\\x1B[38;2;238;130;238m\"\n";
+    file << "#define GOLD    \"\\x1B[38;2;255;215;0m\"\n";
+    file << "#define SILVER  \"\\x1B[38;2;192;192;192m\"\n";
+    file << "#define NAVY    \"\\x1B[38;2;0;0;128m\"\n";
+    file << "#define MAROON  \"\\x1B[38;2;128;0;0m\"\n";
+    file << "#define RESET   \"\\x1b[0m\"\n\n";
+
+    file << "/* ------- EMOJIS ------- */\n";
+    file << "#define HIGHFIVE \"🖐️\"\n\n";
+
+    file << "/* ------- BACKGROUND COLORS ------- */\n";
+    file << "#define BG_BLACK   \"\\x1B[40m\"\n";
+    file << "#define BG_RED     \"\\x1B[41m\"\n";
+    file << "#define BG_GREEN   \"\\x1B[42m\"\n";
+    file << "#define BG_YELLOW  \"\\x1B[43m\"\n";
+    file << "#define BG_BLUE    \"\\x1B[44m\"\n";
+    file << "#define BG_MAGENTA \"\\x1B[45m\"\n";
+    file << "#define BG_CYAN    \"\\x1B[46m\"\n";
+    file << "#define BG_WHITE   \"\\x1B[47m\"\n";
+    file << "#define BG_ORANGE  \"\\x1B[48;2;255;128;0m\"\n";
+    file << "#define BG_PURPLE  \"\\x1B[48;2;128;0;128m\"\n";
+    file << "#define BG_GRAY    \"\\x1B[48;2;176;174;174m\"\n\n";
+
+    file << "/* -------  EFFECTS ------- */\n";
+    file << "#define BOLD      \"\\x1b[1m\"    // Negrita\n";
+    file << "#define DIM       \"\\x1b[2m\"    // Atenuado\n";
+    file << "#define ITALIC    \"\\x1b[3m\"    // Cursiva\n";
+    file << "#define UNDERLINE \"\\x1b[4m\"    // Subrayado\n";
+    file << "#define BLINK     \"\\x1b[5m\"    // Parpadeo\n";
+    file << "#define REVERSE   \"\\x1b[7m\"    // Invertir colores\n";
+    file << "#define HIDDEN    \"\\x1b[8m\"    // Oculto\n";
+    file << "#define STRIKETHROUGH \"\\x1b[9m\" // Tachado\n\n";
+
+    file << "#endif\n";
+
+    file.close();
 }
 
 int main()
@@ -107,8 +214,10 @@ int main()
 		std::cout << "Enter the class name: ";
 		std::cin >> className;
 		createClassHpp(className);
-		createCpp(className);
-		std::cout << "Header file " << className << ".hpp and " << className << ".cpp created successfully!" << std::endl;
+		createClassCpp(className);
+		createMakefile(className);
+		createColorsHpp(className);
+		std::cout << "File " << className << ".hpp, " << className << ".cpp and Makefile created successfully!" << std::endl;
 		i++;
 	}
 
